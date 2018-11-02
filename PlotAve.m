c6ee85b861b55b286e7a0cce1cbbcbf746b8e0ad
Keyvan Zare-Rami <keyvan.zare@gmail.com>
2018-11-02 16:29:59 -0500

    Update PlotAve.m

--- PlotAve.m
+++ PlotAve.m
@@ -22,8 +22,8 @@ figure(10);
 hold on
 plot(AverageX(:,1),AverageX(:,2));
 % title('')
-ylabel('Average Stress (kN/m2)');
-xlabel('Time (s)');
+ylabel('Average Stress');
+xlabel('Time');
 axis([min(AverageX(:,1)) max(AverageX(:,1))...
       min(AverageX(:,2)) max(AverageX(:,2))]);
 hold off
